36590c9787a131bc59e509f4c6b6d8acf28a5393
inception <delaguilarobin5@gmail.com>
2023-02-02 16:09:03 +0100

    mariadbfix

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -5,45 +5,41 @@ if [ ! -d "/run/mysqld" ]; then
         chown -R mysql:mysql /run/mysqld
 fi
 
-#starting mysql
-service mysql start;
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	
+	chown -R mysql:mysql /var/lib/mysql
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_DATABASE_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${WP_DATABASE_USR}\`@'localhost' IDENTIFIED BY '${WP_DATABASE_PWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DATABASE_NAME}\`.* TO \`${WP_DATABASE_USR}\`@'%' IDENTIFIED BY '${WP_DATABASE_PWD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
-mysql -e "FLUSH PRIVILEGES;"
+	# init database
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
+
+cat << EOF > $tfile
+
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+
+CREATE DATABASE $WP_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$WP_DATABASE_USR'@'%' IDENTIFIED by '$WP_DATABASE_PWD';
+GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%';
+
+FLUSH PPRIVILEGES;
+EOF
+
+        /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+        rm -f $tfile
+fi
 
 #reboot mysql to apply the changes
 mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
 exec mysqld_safe
-
-
-# /etc/init.d/mysql start
-
-# if [ -d "/var/lib/mysql/$WP_DATABASE_NAME" ]
-# then 
-#     echo "Database exists - Nothing to be done"
-# else
-#     echo "Database does not exist - Creating database..."
-#     mysql_install_db --user=$MYSQL_USER > /dev/null
-#     mysql_secure_installation << _EOF_
-
-# y
-# $MYSQL_ROOT_PASSWORD
-# $MYSQL_ROOT_PASSWORD
-# y
-# n
-# y
-# y
-# _EOF_
-
-#     echo "CREATE DATABASE IF NOT EXISTS $WP_DATABASE_NAME; GRANT ALL ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-#     mysql -uroot -p$MYSQL_ROOT_PWD $WP_DATABASE_NAME < /usr/local/bin/wordpress.sql 
-#     echo "\n ----- Database configured -----\n"
-
-# fi
-
-# /etc/init.d/mysql stop
-
-# exec "$@"
